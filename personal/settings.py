b2025dc2d5740b802e882603fdacec65fd144230
karem magdy <87768872+kareem333j@users.noreply.github.com>
2023-08-19 18:35:02 +0300

    up

--- personal/settings.py
+++ personal/settings.py
@@ -63,10 +63,20 @@ WSGI_APPLICATION = 'personal.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'t2vpvB0koXGfuD6doPuh',
+        'HOST':'containers-us-west-160.railway.app',
+        'PORT':'7490',
     }
 }
 
